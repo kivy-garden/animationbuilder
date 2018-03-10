cafed9dbc393934c1a55560247682245c509dca8
gottadiveintopython <flow4re2c@mail.com>
2018-03-10 15:49:18 +0900

    Rename: test.py -> test/test.py

--- test/test.py
+++ test/test.py
@@ -3,6 +3,9 @@
 from kivy.lang import Builder
 from kivy.base import runTouchApp
 
+import sys
+import os.path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from animationbuilder import AnimationBuilder
 
 
